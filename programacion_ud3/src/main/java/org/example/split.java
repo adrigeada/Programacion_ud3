e46cc746364771d6401678f0990c29768380da0b
A709\dam1 <adrigeada@gmail.com>
2025-11-07 19:32:55 +0100

    Apuntes split

---
+++ programacion_ud3/src/main/java/org/example/split.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class split {
+    static void main() {
+        String cadena = "Hola,Soy,Una,Cadena";
+        String[] subcadenas = cadena.split(","); //te devuelve un vector tipo string, en cada posicion del vector lo que hay antes de los splits, en este caso comas.
+        System.out.println(Arrays.toString(subcadenas));
+
+     String numeros = "23,4,56,78,80";
+     String[] vector_num = numeros.split(",");
+//        System.out.println(Arrays.toString(vector_num)); //los numeros aqui son strings, con el for paso cada uno a int.
+        int[] num=new int[vector_num.length];
+
+
+        for (int i = 0; i< vector_num.length; i++){
+            num[i] = Integer.parseInt(vector_num[i]);
+
+        }
+        System.out.println(Arrays.toString(num));
+
+
+    }
+}
