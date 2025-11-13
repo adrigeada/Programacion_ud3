32313b89804d25e581eee4cfdd30355c7e83427b
A709\dam1 <adrigeada@gmail.com>
2025-11-13 19:27:13 +0100

    no message

--- programacion_ud3/src/main/java/org/example/ordenar.java
+++ programacion_ud3/src/main/java/org/example/ordenar.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ordenar {
+    static void main() {
+        Integer[] vector = {4,3,2,5,0};
+        Integer[] vector2 = vector.clone();
+
+        Arrays.sort(vector2); //Ordena el vector de manera ascendente
+
+        System.out.println(Arrays.toString(vector2));
+
+        Arrays.sort(vector, Collections.reverseOrder()); //Ordena el vector de manera descendente. Esto necesita que el vector sea Integer, no int.
+
+        System.out.println(Arrays.toString(vector));
+
+        int posicion = Arrays.binarySearch(vector2,3); //si esto sale un numero negativo, es porque el numero que buscas no está en el vector. O porque el vector tiene que ser int no Integer
+        System.out.println(posicion);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
