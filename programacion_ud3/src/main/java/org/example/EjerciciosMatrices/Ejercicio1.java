13b25cf777092e66a22a8725ff6715287eb217fd
A709\dam1 <adrigeada@gmail.com>
2025-11-19 21:15:52 +0100

    Ejercicios Matrices

--- programacion_ud3/src/main/java/org/example/EjerciciosMatrices/Ejercicio1.java
+++ programacion_ud3/src/main/java/org/example/EjerciciosMatrices/Ejercicio1.java
@@ -1,4 +1,73 @@
 package org.example.EjerciciosMatrices;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
+
+        System.out.println("Escribe las filas de la matriz");
+        int n = teclado.nextInt();
+
+        int[][] matriz = new int[n][n];
+
+        //Crear una matriz con numeros aleatorios
+        for (int i = 0 ; i< matriz.length ; i++){
+            for (int j = 0 ; j<matriz[i].length ; j++){
+                matriz[i][j] = aleatorio.nextInt(9)+1;
+
+            }
+        }
+
+        //Imprimir la matriz
+        for (int i = 0 ; i< matriz.length ; i++){
+            for (int j = 0 ; j<matriz[0].length ; j++){
+
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+        }
+
+        //Imprimir la diagonal con un for
+        for (int i = 0 ; i< matriz.length ; i++){
+
+            System.out.print(matriz[i][i]);
+        }
+        System.out.println();
+
+        //Imprimir la diagonal con dos for y un if
+        for(int i = 0; i < matriz.length ; i++){
+            for (int j = 0 ; j<matriz[i].length ; j++){
+                if (i == j){
+                    System.out.print(matriz[i][j]);
+                }
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                if (i+j == matriz.length-1){
+                    System.out.print(matriz[i][j]);
+                }
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- programacion_ud3/src/main/java/org/example/EjerciciosMatrices/Ejercicio2.java
+++ programacion_ud3/src/main/java/org/example/EjerciciosMatrices/Ejercicio2.java
@@ -1,4 +1,51 @@
 package org.example.EjerciciosMatrices;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio2 {
+    static void main() {
+        Random aleatorio = new Random();
+
+        int[][] matriz1 = new int[3][3];
+        int[][] matriz2 = new int[3][3];
+        int[][] matriz_final = new int[3][3];
+
+        for (int i = 0; i < matriz1.length ; i++) {
+            for (int j = 0; j <matriz1[i].length ; j++) {
+                matriz1[i][j] = aleatorio.nextInt(15)+1;
+            }
+        }
+
+        for (int i = 0; i < matriz2.length ; i++) {
+            for (int j = 0; j <matriz2[i].length ; j++) {
+                matriz2[i][j] = aleatorio.nextInt(15)+1;
+            }
+        }
+
+
+        for (int[] fila : matriz1){
+            System.out.println(Arrays.toString(fila));
+        }
+
+        for (int[] fila : matriz2){
+            System.out.println(Arrays.toString(fila));
+        }
+
+        for (int i = 0; i < matriz_final.length ; i++) {
+            for (int j = 0; j <matriz_final[i].length ; j++) {
+                if (matriz1[i][j] > matriz2[i][j]){
+                    matriz_final[i][j] = matriz1[i][j];
+                }else {
+                    matriz_final[i][j] = matriz2[i][j];
+                }
+            }
+        }
+
+        for (int[] fila : matriz_final){
+            System.out.println(Arrays.toString(fila));
+        }
+
+
+    }
 }
