f02cfd6688209be0d65b618d653a74d57b5e095e
A709\dam1 <adrigeada@gmail.com>
2025-11-21 18:36:00 +0100

    Rellenar matriz

--- programacion_ud3/src/main/java/org/example/RellenarMatriz.java
+++ programacion_ud3/src/main/java/org/example/RellenarMatriz.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class RellenarMatriz {
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        String[][] nombres = new String[2][3];
+
+        for (int i = 0; i < nombres.length; i++) {
+
+            System.out.println("Escribe una linea de la matriz");
+            String[] fila = teclado.next().split(";");
+            if (fila.length == nombres[i].length){
+
+                for (int j = 0; j < nombres[i].length; j++) {
+
+                    nombres[i][j] = fila[j];
+                }
+            }else {
+                System.out.println("El tamaño no es el adecuado");
+                i--;
+            }
+
+
+        }
+
+        for (String fila[] : nombres){
+            System.out.println(Arrays.toString(fila));
+        }
+
+
+
+
+    }
 }
